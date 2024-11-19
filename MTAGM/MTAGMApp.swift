c759e601f88ac302abbbc10f86ceb71c9a0dba7f
SeymaArslan <seymarslannn@gmail.com>
2024-11-19 14:42:52 +0300

    Initial Commit

---
+++ MTAGM/MTAGMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MTAGMApp.swift
+//  MTAGM
+//
+//  Created by Seyma Arslan on 19.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct MTAGMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
